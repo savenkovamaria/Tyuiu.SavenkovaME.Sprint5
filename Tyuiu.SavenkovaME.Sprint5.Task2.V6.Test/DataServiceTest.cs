84332fb33f118dd8ec6ccf858f2d527141bbff83
Tyuiu.SavenkovaME@outlook.com <savenkova_mars@mail.ru>
2023-11-16 18:56:08 +0500

    Task2

---
+++ Tyuiu.SavenkovaME.Sprint5.Task2.V6.Test/DataServiceTest.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using Tyuiu.SavenkovaME.Sprint5.Task2.V6.Lib;
+using System.IO;
+
+namespace Tyuiu.SavenkovaME.Sprint5.Task2.V6.Test
+{
+    [TestClass]
+    public class DataServiceTest
+    {
+        [TestMethod]
+        public void SaveToFileTextData()
+        {
+            string path = @"C:\Users\saven\source\repos\Tyuiu.SavenkovaME.Sprint5\Tyuiu.SavenkovaME.Sprint5.Task2.V6\bin\Debug\OutPutFileTask2.csv";
+            FileInfo file = new FileInfo(path);
+            bool exists = file.Exists;
+            bool wait = true;
+            Assert.AreEqual(wait, exists);
+        }
+    }
+}
---
+++ Tyuiu.SavenkovaME.Sprint5.Task2.V6/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Tyuiu.SavenkovaME.Sprint5.Task2.V6.Lib;
+using System.IO;
+
+namespace Tyuiu.SavenkovaME.Sprint5.Task2.V6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DataService ds = new DataService();
+            Console.Title = "Спринт #5 | Выполнила: Савенкова М. Е. | ИИПб-23-2";
+            Console.WriteLine("********************************************************************************");
+            Console.WriteLine("* Спринт #5                                                                    *");
+            Console.WriteLine("* Тема: Класс File. Запись структурированных данных в текстовый файл           *");
+            Console.WriteLine("* Задание #2                                                                   *");
+            Console.WriteLine("* Вариант #6                                                                   *");
+            Console.WriteLine("* Выполнила Савенкова М. Е. | ИИПб-23-2                                        *");
+            Console.WriteLine("********************************************************************************");
+            Console.WriteLine("* УСЛОВИЕ:                                                                     *");
+            Console.WriteLine("* Дан двумерный целочисленный массив 3 на 3 элементов, заполненный значениями  *");
+            Console.WriteLine("* с клавиатуры. Заменить положительные элементы массива на 1, отрицательные    *");
+            Console.WriteLine("* на 0. Результат сохранить в файл OutPutFileTask2.csv и вывести на консоль.   *");
+            Console.WriteLine("* -2; 1; 8                                                                     *");
+            Console.WriteLine("* -4; -7; 8                                                                    *");
+            Console.WriteLine("* 6; 5; 5                                                                      *");
+            Console.WriteLine("********************************************************************************");
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                             *");
+            Console.WriteLine("********************************************************************************");
+            int[,] matrix = new int[3, 3] { { -2, 1, 8 }, { -4, -7, 8 }, { 6, 5, 5 } };
+            int rows = matrix.GetUpperBound(0) + 1;
+            int columns = matrix.Length / rows;
+            for (int i = 0; i < rows; i++)
+            {
+                for (int j = 0; j < columns; j++)
+                {
+                    Console.Write(matrix[i, j] + "\t");
+                }
+                Console.WriteLine();
+            }
+            string res = ds.SaveToFileTextData(matrix);
+            Console.WriteLine("********************************************************************************");
+            Console.WriteLine("* РЕЗУЛЬТАТ:                                                                   *");
+            Console.WriteLine("********************************************************************************");
+            Console.WriteLine("Файл: " + res);
+            Console.WriteLine("Создан!");
+            Console.ReadKey();
+        }
+    }
+}
